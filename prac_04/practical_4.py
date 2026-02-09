7f8c7767d16b2810d3b7182afaa6ff6907a46ba6
fredericknoelshi <fredericknoelwang.shi@my.jcu.edu.au>
2026-02-09 13:40:33 +0800

    not having to use append and learning to use sort and import itemgetter

---
+++ prac_04/practical_4.py
@@ -0,0 +1,41 @@
+# from operator import itemgetter
+#
+# data = [["Lea",34], ["Sam",24],["Eve", 67]]
+#
+# data.sort()
+# print(data)
+#
+# data.sort(key=itemgetter(1))
+# print(data)
+#
+# data.sort(key=itemgetter(1), reverse=True)
+# print(data)
+
+
+# words = "This is Python"
+# print(words)
+#
+# words = words.split()
+# print(words)
+#
+# words.append("is")
+# print(words)
+#
+# print([word for word in words])
+
+
+# numbers = [23, 67, 45, 23]
+# new_numbers = []
+#
+# for number in numbers:
+#     total = number * 2
+#     new_numbers.append(total)
+#
+# print(new_numbers)
+#
+# print([number * 2 for number in numbers])
+# # instead of using append
+# # formula: Action for X in Xs
+#
+# print([number * 2 for number in numbers if number > 50])
+
