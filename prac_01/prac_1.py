1a78cdc0de58ac734093850c032d3c7deef87c68
fredericknoelshi <fredericknoelwang.shi@my.jcu.edu.au>
2026-02-09 14:21:30 +0800

    commit and push

---
+++ prac_01/prac_1.py
@@ -0,0 +1,35 @@
+# def main1():
+#     for i in range(0, 101):
+#         print(f"{i} - {score_grade(i)}")
+#
+# def score_grade(score):
+#     if score >= 85:
+#         grade = "HD"
+#     elif score >= 75:
+#         grade = "D"
+#     elif score >= 65:
+#         grade = "C"
+#     else:
+#         grade = "F"
+#     return grade
+
+# def main2():
+#     import random
+#     high_number = int(input("high number:"))
+#     low_number = int(input("low number:"))
+#     while high_number < low_number:
+#         high_number = int(input("high number:"))
+#         low_number = int(input("low number:"))
+#
+#     random_number = int(random.randint(low_number, high_number))
+#
+#     for i in range(random_number):
+#         print(":)")
+
+# determine_grade(total_score)
+# convert_usd_aud
+# print_report
+# calculate_average(numbers)
+# is_even
+# get_valid_age
+#
